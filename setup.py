a4e1344782fa0fd4158051ddd63bb2f3e5ca985b
Edward S. Vinyard <ed@tzahk.com>
2012-08-08 13:28:06 -0500

    Changed setup.py so that pip-installed version is distinguishable from parent project

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-version = '0.2'
+version = '1.1'
 
 cmdclass = {}
 source_files = ['qrencode.c']
 
 setup(
-    name="pyqrencode",
-    version="0.2",
-    description="Python bindings for libqrencode",
-    author="Matt Reiferson",
-    author_email="mattr@bit.ly",
-    url="http://github.com/bitly/pyqrencode",
-    download_url="http://github.com/downloads/bitly/pyqrencode/pyqrencode-%s.tar.gz" % version,
+    name="pyqrencode_esv",
+    version=version,
+    description="Ed Vinyard's branch of Python bindings for libqrencode",
+    author="Matt Reiferson, Ed Vinyard",
+    author_email="mattr@bit.ly, ed@tzahk.com",
+    url="http://github.com/EdVinyard/pyqrencode",
+    download_url="https://github.com/EdVinyard/pyqrencode/tarball/%s" % version,
     cmdclass=cmdclass,
     ext_modules=[Extension("qrencode", source_files, libraries=["qrencode"])]
 )
